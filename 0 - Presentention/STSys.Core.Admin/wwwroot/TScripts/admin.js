95013127a8f93fdbe016b32b580c9babb67c6d41
git <https://gitee.com/xxtk/NASys.git>
2019-11-22 12:57:47 +0800

    111

---
+++ 0 - Presentention/STSys.Core.Admin/wwwroot/TScripts/admin.js
@@ -0,0 +1,6 @@
+function sayHello() {
+    var compiler = document.getElementById("compiler").value;
+    var framework = document.getElementById("framework").value;
+    return "Hello from " + compiler + " and " + framework + "!";
+}
+//# sourceMappingURL=admin.js.map
